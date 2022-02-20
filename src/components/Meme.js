baaf934e2b466d03501b145ff58f44b21b8e2f8d
Raznor <62889081+Raznor@users.noreply.github.com>
2022-02-20 13:26:23 +0100

    handling text input change with state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,8 +3,8 @@ import memesData from '../data.json';
 
 export default function Meme() {
   const [meme, setMeme] = React.useState({
-    topText: 'TOP TEXT',
-    bottomText: 'BOTTOM TEXT',
+    topText: '',
+    bottomText: '',
     memeImage: getRandomElem(memesData.data.memes).url
   });
   
@@ -18,26 +18,37 @@ export default function Meme() {
       memeImage: getRandomElem(memesData.data.memes).url
     }));
   }
+
+  function handleChange(event) {
+    const {name, value} = event.target;
+    setMeme(prevMeme => ({...prevMeme, [name]: value}));
+  }
   
   return (
     <main className="meme-content">
       <div className="meme-form">
         <input 
           className="input-box input-1"
+          name="topText"
           type="text"
           placeholder="top text"
+          value={meme.topText}
+          onChange={handleChange}
         />
         <input 
           className="input-box input-2"
+          name="bottomText"
           type="text"
           placeholder="bottom text"
+          value={meme.bottomText}
+          onChange={handleChange}
         />
         <button className="new-button" onClick={getMemeImg}>
           <div className="new-button--label">Get a new meme image 🖼</div>
         </button>
       </div>
       <div className="meme-container">
-        <img src={meme.memeImage} className="meme-image" alt="meme image" />
+        <img src={meme.memeImage} className="meme-image" alt="meme" />
         <p className="meme-text top-text">{meme.topText}</p>
         <p className="meme-text bottom-text">{meme.bottomText}</p>
       </div>
